03ba6ffb0ebcebb701383ed0c50798809cc3e6cf
Chufan Deng <jerry.dengcf@gmail.com>
2017-10-24 02:13:23 -0700

    670

---
+++ Array/M_Maximun_Swap.java
@@ -0,0 +1,23 @@
+//670. Maximum Swap
+//index in int buckets[10], 从9到1的value需从小到大
+//medium
+class Solution {
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+        return num;
+    }
+}
