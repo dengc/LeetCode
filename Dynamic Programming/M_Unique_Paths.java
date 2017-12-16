9832612882f61b3f8f17a5c1f67f3565b53beb23
Chufan Deng <jerry.dengcf@gmail.com>
2017-12-16 06:20:24 -0500

    62

---
+++ Dynamic Programming/M_Unique_Paths.java
@@ -0,0 +1,17 @@
+//62. Unique Paths
+//new int[m][n] = 上 + 左
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] res = new int[m][n];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0||j==0)
+                    res[i][j] = 1;
+                else
+                    res[i][j] = res[i][j-1] + res[i-1][j];
+            }
+        }
+        return res[m-1][n-1];
+    }
+}
