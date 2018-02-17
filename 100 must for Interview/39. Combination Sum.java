8d9a41e6306676d7dfcd406decd3fdebd006f5d0
Chufan Deng <jerry.dengcf@gmail.com>
2018-02-16 20:47:06 -0800

    39

---
+++ 100 must for Interview/39. Combination Sum.java
@@ -0,0 +1,25 @@
+//39. Combination Sum
+//先sort，再新method：getResult(result, cur, candidates, target - candidates[i], i);
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    	Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        getResult(result, new ArrayList<Integer>(), candidates, target, 0);
+        
+        return result;
+    }
+    
+    private void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
+    	if(target > 0){
+    		for(int i = start; i < candidates.length && target >= candidates[i]; i++){
+    			cur.add(candidates[i]);
+    			getResult(result, cur, candidates, target - candidates[i], i);
+    			cur.remove(cur.size() - 1);
+    		}
+    	}
+    	else if(target == 0){
+    		result.add(new ArrayList<Integer>(cur));
+    	}
+    }
+}
