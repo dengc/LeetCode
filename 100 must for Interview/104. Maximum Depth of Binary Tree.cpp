55a20ae0af7418213276c337d35110697343a45d
Chufan Deng <jerry.dengcf@gmail.com>
2018-03-19 18:33:58 -0700

    104

---
+++ 100 must for Interview/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,10 @@
+//104. Maximum Depth of Binary Tree
+//max(左，右) + 1
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
