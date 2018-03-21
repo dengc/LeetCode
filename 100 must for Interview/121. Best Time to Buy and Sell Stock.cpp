75e09e943828e68afe27afd65f998bfa66937878
Chufan Deng <jerry.dengcf@gmail.com>
2018-03-20 17:58:16 -0700

    122

--- 100 must for Interview/121. Best Time to Buy and Sell Stock.cpp
+++ 100 must for Interview/121. Best Time to Buy and Sell Stock.cpp
@@ -4,10 +4,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() < 2) return 0;
-        int minNum = prices[0];
+        int minNum = INT_MAX;
         int maxNum = 0;
-        for(int i = 1; i < prices.size(); i++){
+        for(int i = 0; i < prices.size(); i++){
             minNum = min(minNum, prices[i]);
             maxNum = max(maxNum, prices[i] - minNum);
         }
