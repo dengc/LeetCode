9059c08e60dca6decb60bb41f707b5b1f8e8b6aa
Chufan Deng <jerry.dengcf@gmail.com>
2018-03-21 21:13:04 -0700

    134

---
+++ 100 must for Interview/134. Gas Station.java
@@ -0,0 +1,21 @@
+//134. Gas Station
+//tank到某一刻小于0，则清零,start = i+1
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int tank = 0;
+        int sumGas = 0;
+        int sumCost = 0;
+        for(int i = 0; i < gas.length; i++){
+            sumGas += gas[i];
+            sumCost += cost[i];
+            tank += gas[i] - cost[i];
+            if(tank < 0){
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return sumGas<sumCost ? -1:start;
+    }
+}
