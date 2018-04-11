decae4d11a3bddc0af9eb53cca62286134f0174d
Chufan Deng <jerry.dengcf@gmail.com>
2018-04-10 18:51:56 -0700

    287

---
+++ 100 must for Interview/287. Find the Duplicate Number.java
@@ -0,0 +1,19 @@
+//287. Find the Duplicate Number
+//slow & fast: like cycle in LinkedList
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
