51d822c2b4b75e07b803fd17b79179f6c391c481
Chufan Deng <jerry.dengcf@gmail.com>
2018-09-14 01:10:13 -0700

    change 49

--- 100 must for Interview/49. Group Anagrams.java
+++ 100 must for Interview/49. Group Anagrams.java
@@ -4,18 +4,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for(String s : strs){
-        	char[] letters = s.toCharArray();
-        	Arrays.sort(letters);
-        	String key = new String(letters);
+        
+        for(String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
         }
-        if(map.containsKey(key)){
-        	map.get(key).add(s);
-        }else {
-        	List<String> list = new ArrayList<>();
-        	list.add(s);
-        	map.put(key, list);
-        }
+        
         return new ArrayList<>(map.values());
     }
 }
